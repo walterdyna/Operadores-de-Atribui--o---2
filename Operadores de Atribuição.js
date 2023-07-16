2d5e66733904ef2db7e8692450e88e0691a5275d
walterdyna <alexdyna@gmail.com>
2023-07-16 15:01:57 -0300

    Operadores de Atribuição

---
+++ Operadores de Atribuição.js
@@ -0,0 +1,41 @@
+
+/*
+Operadores de Atribuição
+
+   =   --> Atribuição
+   +=  --> Adição
+   -=  --> Subtração
+   /=  --> Divisão
+   *=  --> Multiplicação
+   %=  --> Resto
+
+   */
+
+const result = 20
+console.log(result)
+console.log("-----------------------------------------")
+
+let resultado = 33
+resultado += 70 // e igual a  ----- > resultado = resultado + 70
+console.log(resultado)
+console.log("-----------------------------------------")
+
+let subtracao = 30
+subtracao -= 80 // e igual a  -----> subtração = subtração - 80
+console.log(subtracao)
+console.log("-----------------------------------------")
+
+let divisao = 20
+divisao /= 2 // e igual a  -----> divisao = divisao / 2
+console.log(divisao)
+console.log("-----------------------------------------")
+
+let multiplicacao = 10
+multiplicacao *= 2 // e igual a  -----> multiplicacao = multiplicacao *2
+console.log(multiplicacao)
+console.log("-----------------------------------------")
+
+let resto = 23
+resto %= 2 // e igual a -----> resto = resto % 2
+console.log(resto)
+console.log("-----------------------------------------")
